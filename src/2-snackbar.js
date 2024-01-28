c5b5bb42af2246232b54833bb39515eff99ee339
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-01-28 16:24:53 +0000

    Update task #2

---
+++ src/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+
+const delayInput = document.querySelector('[name="delay"]');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = delayInput.value;
+  createPromise(delay, form.elements.state.value)
+    .then(delay => {
+      iziToast.show({
+        title: 'Hey',
+        message: `Fulfilled promise in ${delay} ms`,
+        closeOnClick: true,
+        closeOnEscape: true,
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        title: 'Hey',
+        message: `Rejected promise in ${delay} ms`,
+        closeOnClick: true,
+        closeOnEscape: true,
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (form.elements.state.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  return promise;
+}
